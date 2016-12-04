c38f1b90183435f3cbf4068d54fb7708868836c3
virgolus <virgolus@gmail.com>
2016-12-04 15:00:10 +0000

    Modifica script test wifi

--- WarmMe/wifi/wifiController.sh
+++ WarmMe/wifi/wifiController.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-if iwconfig wlan0 | grep "ESSID:off" > /dev/null
+
+while [ : ]
+do
+wifiup=$(/sbin/ifconfig wlan0 | grep inet\ addr | wc -l)
+if [ $wifiup -eq 0 ]
 then
 	echo "link is down, restaring wlan0..."
 	ifdown wlan0
 	sleep 5s
 	ifup wlan0
-	sudo iwconfig wlan0 power off
-	sleep 30s
+	#iwconfig wlan0 power off
+	sleep 10s
 	dhclient wlan0
 else
 	echo "link is up"
 fi
-exit 0
+sleep 1m
+done
